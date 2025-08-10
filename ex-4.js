b279687a8aa4faac499a7b3cffac31e60336b262
En <graphic.mimage@gmail.com>
2025-08-10 14:29:05 +0700

    exam4 : Add Name (Alicia & Kody) in Oject

--- ex-4.js
+++ ex-4.js
@@ -12,3 +12,28 @@ const employees = [
 ];
 
 // Start coding here
+const Alicia = [
+  {
+    name: "Alicia",
+    age: 29,
+    hobbies: ["Shopping" , "Reading novles"]
+  }
+]
+
+const kody = [{
+  name: "Kody",
+  age: 19,
+  hobbie: ["Computer games" , "Wakeboard"]
+}]
+
+employees.push(Alicia,kody);
+
+console.log(employees);
+
+// Key name มี Value เป็น "Alicia"
+// Key age มี Value เป็น 29
+// Key hobbies มี Value เป็น “Shopping" และ “Reading novels"
+// พนักงานคนที่ 2 มี Property ของ Object ดังนี้
+// Key name มี Value เป็น "Kody"
+// Key age มี Value เป็น 19
+// Key hobbies มี Value เป็น “Computer games" และ “Wakeboard"
